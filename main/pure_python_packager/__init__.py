dbab7a87ae19005a0f44f98d79ea0257cdc28e09
Jeff Hykin <jeff.hykin@gmail.com>
2023-04-18 12:17:24 -0500

    work on add_dependency

--- main/pure_python_packager/__init__.py
+++ main/pure_python_packager/__init__.py
@@ -6,7 +6,9 @@ from pathlib import Path
 from os.path import join, dirname
 import warnings
 import json
+from .__dependencies__ import toml
 
+default_indent = 4
 def make_absolute_path(to, coming_from=None):
     # if coming from cwd, its easy
     if coming_from is None:
@@ -57,11 +59,13 @@ def get_original_dependency_initer():
 
 
 # TODO: check git subrepo command        
-def add_dependency(project_directory, url, module_name=None, module_path=None, extraction_eval=None):
+def add_dependency(project_directory, url, commit_or_tag=None, module_name=None, module_path=None, extraction_eval=None):
+    # FIXME: allow installing a specific tag
+    
+    *_, url_ending_name, _ = path_pieces(url)
     # detect module name
     if module_name == None:
-        *folders, name, extension = path_pieces(url)
-        module_name = name
+        module_name = url_ending_name
     
     path_to_init = join(project_directory, "__dependencies__", "__init__.py")
     path_to_dependency_map = join(project_directory, "__dependencies__", "__dependency_mapping__.json")
@@ -84,11 +88,37 @@ def add_dependency(project_directory, url, module_name=None, module_path=None, e
         if not isinstance(dependency_mapping, dict):
             raise Exception(f"""\n\n\nThis file is corrupt (it should be a JSON object):{path_to_dependency_map}""")
     
-    # TODO: git subrepo clone
+    repo_path = f"{url_ending_name}_{hash(f"{commit_or_tag}{url}")}"
     
-    # TODO: check the toml
-    # if 
-
-    # TODO: convert "from [self].thing1 import thing2" to "from .thing1 improt thing2"
-
+    # TODO: git subrepo clone
+    # FIXME: checkout commit_or_tag
+    
+    if not module_path:
+        main_module_path = None
+        toml_path = join(repo_path, "pyproject.toml")
+        if isfile(toml_path):
+            toml_info = toml.load(toml_path)
+            try:
+                main_module_path = toml_info["tool"]["extra"]["main_module_path"]
+            except Exception as error:
+                pass
+        if main_module_path == None:
+            typical_module_path = join(repo_path, module_name)
+            if isdir(typical_module_path) and isfile(join(repo_path, "setup.py")):
+                print(f"guessing that: {typical_module_path} is source code folder within the repo")
+                main_module_path = typical_module_path
+        if main_module_path == None:
+            raise Exception(f'''Please give a module_path argument, as I was unable to auto-detect the module path for {module_name}''')
+    
+    # 
+    # update the dependency mapping
+    # 
+    dependency_mapping[module_name] = {
+        "path": make_relative_path(to=main_module_path, coming_from=join(repo_path, "..")),
+        "eval": extraction_eval or module_name,
+    }
+    with open(path_to_dependency_map, 'w') as outfile:
+        json.dump(dependency_mapping, outfile, indent=default_indent)
+            
+    # TODO: convert "from [self].thing1 import thing2" to "from .thing1 improt thing2" within the repo
 
